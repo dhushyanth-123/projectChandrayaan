4e4b9979804945a6a275643b1f7f2d45ed6fb385
Dhushyanth <124496002+dhushyanth-123@users.noreply.github.com>
2023-08-27 23:04:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const button1 = document.getElementById("myButton1");
 const button2 = document.getElementById("myButton2");
 button1.addEventListener("click", function() {window.open("https://en.wikipedia.org/wiki/Chandrayaan-3","_blank");});
-button2.addEventListener("click", function() {window.location.href="go.html";});
+button2.addEventListener("click", function() {window.location.href="projectChandrayaan/go.html";});
